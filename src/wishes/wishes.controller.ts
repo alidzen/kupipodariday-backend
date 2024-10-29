9dfe0e5fe3266e50b4549091500c68edc993232a
Denis Golubtsov <d.s.golubtsov@gmail.com>
2024-10-29 11:22:59 +0100

    fix lint warnings

--- src/wishes/wishes.controller.ts
+++ src/wishes/wishes.controller.ts
@@ -8,8 +8,6 @@ import {
   Body,
   UseGuards,
   Request,
-  ForbiddenException,
-  NotFoundException,
 } from '@nestjs/common';
 import { WishesService } from './wishes.service';
 import { CreateWishDto } from './dto/create-wish.dto';
